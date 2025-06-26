61ce0d5b94fd54237b96b06a5caf22dc9545b773
Richard <richardruizs1998@gmail.com>
2025-06-25 18:37:30 -0600

    Segundo Commit

---
+++ EjercicioPractico1_RIchardRuiz/src/main/java/Farmacia/Web/TiendaRichardApplication.java
@@ -0,0 +1,13 @@
+package Farmacia.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRichardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRichardApplication.class, args);
+	}
+
+}
